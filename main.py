f133a9055c82fc1753c450df97463314f02b8071
Agnieszka Piórkowska <61748687+AgniechaP@users.noreply.github.com>
2023-03-07 19:52:12 +0100

    Add to repo

---
+++ main.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+"""code template"""
+
+import numpy as np
+
+
+def main():
+    #Zadanie 1. - łączne prawdopodobieństwo jako tablica numpy
+    # ([
+    #    [[ tootache & cavity], [~toothache & cavity]],
+    #    [[ tootache & ~cavity], [~toothache & ~cavity]] ])
+    P = np.array([[[0.108, 0.012], [0.072, 0.008]],
+                 [[0.016, 0.064], [0.144, 0.576]]])
+    print("1. Tablica - prawdopodobienstwo laczne:\n", P)
+
+    #Zadanie 2. P(Toothache)
+    #axis 0 -> cavity, axis 1 -> toothache, axis 2 -> catch
+    P_too = np.sum(P, axis=(0,2))
+    print(P_too)
+
+if __name__ == '__main__':
+    main()
