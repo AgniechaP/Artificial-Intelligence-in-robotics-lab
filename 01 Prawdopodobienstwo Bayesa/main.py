a0a23ddfefcc100af6136aafb0f66cc29f25e4d1
AgniechaP <agnpior@gmail.com>
2023-03-08 17:36:47 +0100

    Zadanie 9

--- 01 Prawdopodobienstwo Bayesa/main.py
+++ 01 Prawdopodobienstwo Bayesa/main.py
@@ -14,10 +14,17 @@ def main():
                  [[0.016, 0.064], [0.144, 0.576]]])
     print("1. Tablica - prawdopodobienstwo laczne:\n", P)
 
-    #Zadanie 2. P(Toothache)
+    #Zadanie 2. P(Toothache) wykorzystac mozemy wzor na prawdopodobienstwo brzegowe, marginalizujemy to co niepotrzebne, suma odbywa się po zmiennych, których chcemy się pozbyć
     #axis 0 -> cavity, axis 1 -> toothache, axis 2 -> catch
     P_too = np.sum(P, axis=(0,2))
     print("P_too = ", P_too)
+
+    # Zadanie 2. z zajęć:
+    P_cav_too = np.sum(P,axis=-1)
+    # P_too = np.sum(P_cav_too, axis=0)
+
+    #Zadanie 3. z zajęć:
+    # P_cav = np.sum(P_cav_too, axis=-1)
 
     #Zadanie 3. P(Cavity)
     P_cav = np.sum(P, axis=(1,2))
@@ -26,6 +33,10 @@ def main():
     #Zadanie 4. P(Toothache|Cavity)
     P_toothache_giv_cavity = np.sum(P, axis=2) / P_cav
     print("P_toothache_giv_cavity = \n", P_toothache_giv_cavity)
+
+    #Zadanie 4. Z zajęć: z reguly iloczynu bierzemy czlon odpowiadajacy za prawdopodobienstwo warunkowe
+    P_too_giv_cav_z_zajec = P_cav_too.T/P_cav #Dokonujemy transpozycji, bo liczylismy P(b|a) a we wzorze mamy P(a|b)
+    print("P_too_giv_cav_z_zajec: ", P_too_giv_cav_z_zajec)
 
     #Zadanie 5. P(Cavity|toothache v catch)
     licznik = P_cav - P[:,1,1]
@@ -37,7 +48,7 @@ def main():
     #Wielkosc tablicy wyraza sie jako 2^liczba zmiennych. Tu 2^3 = 8 i jest to wielkosc tablicy
 
     #Zadanie 7. Przechowywanie tablicy dla 32 zmiennych zapisujac liczby jako 23 bitowy float
-    bits = 32 * 2**32
+    bits = 32 * 2**32 #ok. 16 GB
     print("Rozmiar (bity): ", bits)
 
     #Zadanie 8. P(Cavity | Tootache, Catch)
@@ -50,7 +61,11 @@ def main():
     P_too_cat_giv_cav = P/np.reshape(P_cav, (2, 1, 1))
     print("P_too_cat_giv_cav ", P_too_cat_giv_cav)
 
-    #TODO continue
+    P_too_cat = np.sum(P, axis=0)
+    print("P_too_cat: ", P_too_cat)
+
+    P_cav_giv_too_cat = P/P_too_cat
+    print("P_cav_giv_too_cat ",P_cav_giv_too_cat)
 
     #Zadanie 9.
 
